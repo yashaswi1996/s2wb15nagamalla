cc4e4029bb3eec9b5921863417cc7bfc9c75a558
yashaswi1996 <46136157+yashaswi1996@users.noreply.github.com>
2022-04-06 17:51:26 -0500

    comit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var Olives = require('./routes/Olives');
+var OlivesRouter = require('./routes/Olives');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Olives', Olives);
+app.use('/Olives', OlivesRouter);
 
 
 // catch 404 and forward to error handler
