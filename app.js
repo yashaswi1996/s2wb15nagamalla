d0a0dca6c7695bae7973fd784cd46b863a3f6041
yashaswi1996 <46136157+yashaswi1996@users.noreply.github.com>
2022-04-06 18:42:10 -0500

    query strings

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var OlivesRouter = require('./routes/Olives');
+var addmodsRouter = require('./routes/addmods');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Olives', OlivesRouter);
+app.use('/addmods', addmodsRouter);
 
 
 // catch 404 and forward to error handler
