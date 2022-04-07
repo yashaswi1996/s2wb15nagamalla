64637ce111493dfe99e7a24f8bdc9f0040528eda
yashaswi1996 <46136157+yashaswi1996@users.noreply.github.com>
2022-04-06 20:41:18 -0500

    Added selector end point

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var OlivesRouter = require('./routes/Olives');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Olives', OlivesRouter);
 app.use('/addmods', addmodsRouter);
-
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
